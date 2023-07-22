9974887d115cddfe06eac4119a2da6cce97598bb
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-07-21 18:35:22 -0700

    Complete assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w", encoding="utf-8") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a", encoding="utf-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding="utf-8") as file:
+        file_content = file.read()
+        return file_content
